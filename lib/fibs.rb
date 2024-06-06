3576247223de74901e3cc80eb9a6382f57c27e3f
Dostendite <dostendite@outlook.es>
2024-06-06 19:28:30 +0200

    Implement fibs.rb (rudimentary)

---
+++ lib/fibs.rb
@@ -0,0 +1,29 @@
+def fibs(n, ary=[])
+    a = 0
+    b = 1
+    c = a + b
+
+    if n > 0
+        if n > 1
+            ary << a
+            ary << b
+        else
+            ary << a
+        end
+    end
+
+    (n-2).times do
+        c = a + b
+        a = b
+        b = c
+        ary << c
+    end
+    ary
+end
+
+# Tests
+puts "fibs(0) -> #{fibs(0)}"
+puts "fibs(1) -> #{fibs(1)}"
+puts "fibs(2) -> #{fibs(2)}"
+puts "fibs(5) -> #{fibs(5)}"
+puts "fibs(8) -> #{fibs(8)}"

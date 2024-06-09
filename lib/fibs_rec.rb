162ab9486c165ca21b5ad785b513790bc7dcf4da
Dostendite <dostendite@outlook.es>
2024-06-09 16:59:05 +0200

    Implement fibs_rec method

---
+++ lib/fibs_rec.rb
@@ -0,0 +1,21 @@
+def fibs_rec(n, ary=[0])
+    # Return ary with n fibonacci numbers
+    if !ary.include?(1) && n > 1
+        ary << 1
+        n -= 1
+    end
+
+    if n <= 1
+        ary
+    else
+        ary << ary[-1] + ary[-2]
+        fibs_rec(n-1, ary)
+    end
+end
+
+print fibs_rec(1)
+puts
+print fibs_rec(6)
+puts
+print fibs_rec(8)
+puts
